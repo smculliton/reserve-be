b49dc15f255aec6db9e47e2369328dfbb679ab99
Sean Culliton <smculliton@gmail.com>
2023-06-05 16:01:59 -0600

    Adjust for CORS

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,13 @@ module ReserveBe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete]
+       end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
