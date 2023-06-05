32815483d04e78d3806162214e5021a11c00d35e
Sean Culliton <smculliton@gmail.com>
2023-06-05 15:56:58 -0600

    Create seeds

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Reservation.create!(email: 'scull@att.net', party_size: 1, time: DateTime.now)
+Reservation.create!(email: 'scull@att.net', party_size: 1, time: DateTime.now + 7)
+Reservation.create!(email: 'scull@att.net', party_size: 1, time: DateTime.now + 5)
+Reservation.create!(email: 'scull@att.net', party_size: 1, time: DateTime.now + 3)
