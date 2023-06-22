d776ae15e73d50a75a68fb3afbdd4eab51943b36
Sean Culliton <smculliton@gmail.com>
2023-06-22 16:37:56 -0600

    Create restaurant hours parser (WIP)

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RestaurantsController < ApplicationController
   def index
     restaurants = RestaurantsFacade.get_all_restaurants
     restaurants = restaurants.select { |r| r.cuisine.include?(params[:cuisine]) } if params[:cuisine]
-
+    
     render json: RestaurantSerializer.new(restaurants)
   end
 end
--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -4,7 +4,31 @@ class Restaurant
   def initialize(data)
     @name = data[:properties][:name]
     @cuisine = data[:properties][:datasource][:raw][:cuisine] || 'vegan'
-    @hours = data[:properties][:datasource][:raw][:opening_hours]
+    @hours = parse_hours(data[:properties][:datasource][:raw][:opening_hours])
     @id = data[:properties][:datasource][:raw][:osm_id]
+  end
+
+  def parse_hours(hours_string)
+    hours_string = ('Mo-Su 12:00-20:00') if hours_string == nil
+
+    array = hours_string.split(', ').map { |y| y.split(' ') }
+    array = array.map do |a|
+      hash = {}
+      if a[0].include?('-')
+        get_day_range(a[0]).each { |d| hash[d] = a[1] }
+      else
+        hash[a[0]] = a[1]
+      end
+      hash
+    end
+    array.reduce({}, :merge)
+  end
+
+  def get_day_range(str)
+    range = str.split('-')
+    dow = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+    dow = dow.rotate(-1) if range[0] == 'Su'
+
+    dow[dow.index(range[0])..dow.index(range[1])]
   end
 end
